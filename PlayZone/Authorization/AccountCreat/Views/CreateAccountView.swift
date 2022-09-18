8f472a0b1e678ede093fa7f54c78c1a8aa06bad4
1Nod1r <kh.nodirbeek@gmail.com>
2022-09-18 22:24:38 +0500

    Test

---
+++ PlayZone/Authorization/AccountCreat/Views/CreateAccountView.swift
@@ -0,0 +1,12 @@
+//
+//  CreateAccountView.swift
+//  PlayZone
+//
+//  Created by Nodirbek Khudoyberdiev on 17/09/22.
+//
+
+import UIKit
+
+class CreateAccountView: BaseView {
+
+}
---
+++ PlayZone/Extensions/UILabel + Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UILabel + Ext.swift
+//  PlayZone
+//
+//  Created by Nodirbek Khudoyberdiev on 17/09/22.
+//
+
+import Foundation
